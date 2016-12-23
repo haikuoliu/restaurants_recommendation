26c08d569b6249bda70bf3402da2231024b47a43
JisongLiu <jl4819@columbia.edu>
2016-12-23 01:09:05 -0500

    Update DaoLayer.py

--- utils/DaoLayer.py
+++ utils/DaoLayer.py
@@ -17,21 +17,21 @@ def getByEmail(email):
     print "findtheemail  "
     print email
     try:
-        document = userCollection.find_one({"email": email})
+        document = userMongoData.find_one({"email": email})
         return document
     except Exception:
         return "Email incorrect"
 def insertData(data):
     # you should assign an id at first or MongDB will assign a random one for you.
     try:
-        post_id = userCollection.insert_one(data).inserted_id
+        post_id = userMongoData.insert_one(data).inserted_id
         return post_id
     except Exception:
         return "insert fail"
 
 def updateData(user_id, target,value):
     # target is the name of attribute and value is the assigned value
-    update = userCollection.find_one({"user_id": user_id})
+    update = userMongoData.find_one({"user_id": user_id})
     try:
         update[target] = value
         return "update correct"
