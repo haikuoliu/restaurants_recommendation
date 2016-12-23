1dc79124dcd9ca609220f1824a0344c2dda38165
JisongLiu <jl4819@columbia.edu>
2016-12-22 21:58:28 -0500

    Update load.py

--- utils/load.py
+++ utils/load.py
@@ -4,11 +4,16 @@ client = MongoClient()
 db = client.test
 businessMongoData = db.businessMongoData
 fin = open('/home/ec2-user/restaurants_recommendation/data／businessMongoData.json','r')
+count = 0
 for eachLine in fin:
     line = eachLine.strip().decode('utf-8')
     line = line.strip(',')
+    count = count +1
     try:
         js = json.loads(line)
     except Exception, e:
         continue
     businessMongoData.insert_one(js)
+    if count>100:
+        print 'processing'
+        count = 0
