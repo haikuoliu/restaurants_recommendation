be2f4cbbe54a5a8a1cd4ab79739efca819c58951
JisongLiu <jl4819@columbia.edu>
2016-12-22 22:55:34 -0500

    Update count.py

--- utils/count.py
+++ utils/count.py
@@ -4,6 +4,6 @@ db = client.test
 businessMongoData1 = db.businessMongoData1
 userMongoData = db.userMongoData
 c1 = businessMongoData1.count()
-c2 = userMongoDatauser.count()
+c2 = userMongoData.count()
 print c1
 print c2
