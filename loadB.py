1924afd19cc1266acf851508d9afbba49b4aa68b
JisongLiu <jl4819@columbia.edu>
2016-12-23 14:21:50 -0500

    Update loadB.py

--- loadB.py
+++ loadB.py
@@ -2,7 +2,7 @@ import json
 from pymongo import MongoClient
 client = MongoClient()
 db = client.test
-businessMongoData2 = db.businessMongoData2
+businessMongoData3 = db.businessMongoData3
 fin = open('/home/ec2-user/restaurants_recommendation/data/businessMongoData2.json','r')
 count = 0
 for eachLine in fin:
@@ -13,7 +13,7 @@ for eachLine in fin:
         js = json.loads(line)
     except Exception, e:
         continue
-    businessMongoData2.insert_one(js)
+    businessMongoData3.insert_one(js)
     if count>100:
         print 'processing'
         count = 0
