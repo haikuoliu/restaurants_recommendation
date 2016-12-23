09677632e7160958b1feadd1a90ebad0f0231dfa
JisongLiu <jl4819@columbia.edu>
2016-12-22 22:52:05 -0500

    Update count.py

--- utils/count.py
+++ utils/count.py
@@ -1,9 +1,9 @@
 from pymongo import MongoClient
 client = MongoClient()
 db = client.test
-businessMongoData = db.businessMongoData
-# userMongoData = db.userMongoData
-c1 = businessMongoData.count()
-# c2 = userMongoDatauser.count()
+businessMongoData1 = db.businessMongoData1
+userMongoData = db.userMongoData
+c1 = businessMongoData1.count()
+c2 = userMongoDatauser.count()
 print c1
-# print c2
+print c2
