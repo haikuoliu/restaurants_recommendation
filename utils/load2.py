8582ac7ee9b7cfbaec002ef2cbd84f523cedda01
JisongLiu <jl4819@columbia.edu>
2016-12-22 22:04:10 -0500

    Create load2.py

---
+++ utils/load2.py
@@ -0,0 +1,19 @@
+import json
+from pymongo import MongoClient
+client = MongoClient()
+db = client.test
+businessMongoData = db.businessMongoData
+fin = open('/home/ec2-user/restaurants_recommendation/data/userMongoData.json','r')
+count = 0
+for eachLine in fin:
+    line = eachLine.strip().decode('utf-8')
+    line = line.strip(',')
+    count = count +1
+    try:
+        js = json.loads(line)
+    except Exception, e:
+        continue
+    businessMongoData.insert_one(js)
+    if count>100:
+        print 'processing'
+        count = 0
