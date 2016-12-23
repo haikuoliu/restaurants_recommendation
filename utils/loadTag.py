a32a9c7e4e1b702d14d9e116c6f081f4c7bd614e
JisongLiu <jl4819@columbia.edu>
2016-12-23 03:12:00 -0500

    Update loadTag.py

--- utils/loadTag.py
+++ utils/loadTag.py
@@ -2,8 +2,8 @@ import json
 from pymongo import MongoClient
 client = MongoClient()
 db = client.test
-userMongoData = db.userMongoData
-fin = open('/home/ec2-user/restaurants_recommendation/data/userMongoData.json','r')
+userTag = db.userTag
+fin = open('/home/ec2-user/restaurants_recommendation/data/userTag.json','r')
 count = 0
 for eachLine in fin:
     line = eachLine.strip().decode('utf-8')
@@ -13,7 +13,7 @@ for eachLine in fin:
         js = json.loads(line)
     except Exception, e:
         continue
-    userMongoData.insert_one(js)
+    userTag.insert_one(js)
     if count>100:
         print 'processing'
         count = 0
