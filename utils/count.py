ea8c924e3a89fe0212102ce937d51d0de0c063dd
JisongLiu <jl4819@columbia.edu>
2016-12-23 00:42:16 -0500

    Update count.py

--- utils/count.py
+++ utils/count.py
@@ -5,5 +5,8 @@ businessMongoData1 = db.businessMongoData1
 userMongoData = db.userMongoData
 c1 = businessMongoData1.count()
 c2 = userMongoData.count()
+s= "4U9kSBLuBDU391x6bxU-YA"
+c3 = userMongoData.find_one({"user_id": s})
 print c1
 print c2
+print c3
