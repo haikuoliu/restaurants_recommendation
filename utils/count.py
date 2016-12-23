d83b169de6fed2fdda4d448c4db3d28d75a71347
JisongLiu <jl4819@columbia.edu>
2016-12-22 22:26:36 -0500

    Create count.py

---
+++ utils/count.py
@@ -0,0 +1,8 @@
+client = MongoClient()
+db = client.test
+businessMongoData = db.businessMongoData
+userMongoData = db.userMongoDatauser
+c1 = businessMongoData.count()
+c2 = userMongoDatauser.count()
+print c1
+print c2
