8052fb87db73351c9af4acf50c5ff2226fc73176
JisongLiu <jl4819@columbia.edu>
2016-12-24 10:42:26 -0500

    Update count.py

--- utils/count.py
+++ utils/count.py
@@ -1,7 +1,7 @@
 from pymongo import MongoClient
 client = MongoClient()
 db = client.test
-businessMongoData2 = db.businessMongoData2
+businessMongoData2 = db.preUserMongoData2
 userMongoData = db.userMongoData
 userTag = db.userTag
 businessMongoData3 = db.businessMongoData3
@@ -9,7 +9,7 @@ c1 = businessMongoData2.count()
 c2 = userMongoData.count()
 c3 = userTag.count()
 c4 = businessMongoData3.count()
-print "the number of businessMongoData: "
+print "the number of pre: "
 print c1
 print '\n'
 print "the number of businessMongoData: "
