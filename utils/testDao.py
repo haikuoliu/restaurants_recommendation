8930d7a9a48cfac8789c10a6e75c9869a0763588
JisongLiu <jl4819@columbia.edu>
2016-12-24 10:56:50 -0500

    Update testDao.py

--- utils/testDao.py
+++ utils/testDao.py
@@ -3,6 +3,7 @@ client = MongoClient()
 # client set up
 db = client.test
 userMongoData = db.perUserMongoData2
+print userMongoData.count()
 email = "2@gmail.com"
 document = userMongoData.find_one({"email": email})
 print document
